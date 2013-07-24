7479b0ce8a716e0410427379f33562cd70e28893
Sohel Amin <sohelamincse@gmail.com>
2013-07-24 21:56:03 +0600

    just commit

--- geoplugin.class.php
+++ geoplugin.class.php
@@ -1,30 +1,16 @@
 <?php
-/*
+/**
 This PHP class is free software: you can redistribute it and/or modify
 the code under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version. 
-
-However, the license header, copyright and author credits 
-must not be modified in any form and always be displayed.
-
-This class is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-@author geoPlugin (gp_support@geoplugin.com)
-@copyright Copyright geoPlugin (gp_support@geoplugin.com)
-$version 1.01
-
-
-This PHP class uses the PHP Webservice of http://www.geoplugin.com/ to geolocate IP addresses
-
-Geographical location of the IP address (visitor) and locate currency (symbol, code and exchange rate) are returned.
-
-See http://www.geoplugin.com/webservices/php for more specific details of this free service
-
-*/
+ * OOP PHP Class
+ * @category		IP Trace
+ * @author			Sohel Amin
+ * @email			sohelamincse@gmail.com
+ * @author url		http://www.sohelamin.com
+ * @company url		http://www.appzcoder.com
+ */
 
 class geoPlugin {
 	
